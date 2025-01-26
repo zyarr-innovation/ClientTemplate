c2a33ded54ae8aec212b446d13f07f5dfa9d8669
zyarr-innovation <zyarr.innovation@gmail.com>
2025-01-26 03:27:34 -0800

    Removed unnecessary Files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
+import { SchoolComponent } from './school/school.component';
 import { StudentComponent } from './student/student.component';
 
 export const routes: Routes = [
-  { path: '', component: StudentComponent },
+  { path: '', component: SchoolComponent },
+  { path: 'school', component: SchoolComponent },
   { path: 'student', component: StudentComponent },
+  { path: 'student/school/:schoolId', component: StudentComponent },
 ];
